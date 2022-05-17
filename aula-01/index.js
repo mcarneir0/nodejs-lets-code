b1cd94dd4ec34b0ac98c621b368c74c6e6eb21bc
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-05-16 21:50:04 -0300

    Atualizado Aula 01

--- aula-01/index.js
+++ aula-01/index.js
@@ -1,47 +1,68 @@
-console.log('Hello World');
+// useEffect(() => {
+//   // buscar na API
+//   const result = await fetch('https://');
 
-// callback
+// }, []);
 
-console.log('Passo 1');
+// console.log('Hello world!');
 
-function anyComplex(a, b, callback) {
-    const c = a + b;
-    setTimeout(() => {
-        callback(c);
-    }, 2000);    
-};
+// - callback
+// console.log('Passo 1');
 
-console.log('Passo 2');
+// function anyComplex(a, b, callback) {
+//   const c = a + b;
+//   setTimeout(() => {
+//     callback(c);
+//   }, 2000);
+// }
 
-anyComplex(10, 5, (result) => {
-    console.log('Resultado:', result);
-    
-    anyComplex(result, 10, (result2) => {
-        console.log('Resultado 2:', result2);
-    })
-});
+// console.log('Passo 2');
+// // Funções aninhadas
+// anyComplex(10, 5, (result) => {
+//   console.log('Resultado: ' + result);
 
-console.log('Passo 3');
+//   anyComplex(result, 10, (result2) => {
+//     console.log('Resultado 2: ' + result2);
+//   });
+// });
+// console.log('Passo 3');
 
-// promise
-
-new Promise((resolve,err) => {
-    const c = 10 + 5;
-    resolve(c);
-})
-    .then((result) => {
-        console.log('Resultado Promise:', result);
-})
-    .finally(() => {
-        console.log('Acabou!');
-});
-
-// async/await
-
-async function getAsync() {
-    // bater numa API e buscar dados
-    const result = await fetch();
-}
-
-// não pode ter async await na raiz, nesse caso tem que usar a sintaxe de Promise ".then"
-getAsync().then;
+// - promise
+function anyComplexPromise(a, b) {
+    return new Promise((onSuccess, onError) => {
+      // try {
+      const c = 10 + 5;
+      // aqui demora pra executar
+      onSuccess(c);
+      // } catch (err) {
+      //   onError(err);
+      // }
+    });
+  }
+  
+  // anyComplexPromise(10, 5)
+  //   .then((result) => {
+  //     console.log('Resultado promise: ' + result);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+  
+  // - async/await
+  async function calculateAsync() {
+    try {
+      const resultAsync = await anyComplexPromise(10, 5);
+      console.log('Resultado async/await: ' + resultAsync);
+    } catch (err) {
+      console.log(err);
+    } finally {
+    }
+  }
+  
+  async function calculateAsyncWithPromise() {
+    calculateAsync().then(() => {
+      console.log('Terminou!');
+    });
+  }
+  
+  calculateAsyncWithPromise();
